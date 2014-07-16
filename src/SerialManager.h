e882ec13d3236a9d018ea4c6c70e051e9362ac7b
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2014-07-16 15:48:34 +0200

    Serial port reading test

--- src/SerialManager.h
+++ src/SerialManager.h
@@ -6,23 +6,25 @@
 class SerialManager : public QThread
 {
 	public:
-		QSerialPort port{QString("ttymxc3")};
+		QSerialPort* port;//{QString("ttymxc3")};
 		SerialManager()
 		{
-			port.setBaudRate(115200);
+			//port.setBaudRate(115200);
 		}
 
 	protected:
 		virtual void run()
 		{
-			port.open(QIODevice::ReadOnly);
+			port = new QSerialPort("ttymxc3");
+			port->setBaudRate(115200);
+			port->open(QIODevice::ReadOnly);
 			QByteArray line;
 
 			forever
 			{
-				if(port.canReadLine())
+				if(port->canReadLine())
 				{
-					parse(QString(port.readLine(64)));
+					parse(QString(port->readLine(64)));
 				}
 			}
 		}
