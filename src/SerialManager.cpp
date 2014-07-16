5228158ed535b113c94b5005a816c0345dda68db
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2014-07-16 16:30:53 +0200

    Serial port reading test

--- src/SerialManager.cpp
+++ src/SerialManager.cpp
@@ -1,8 +1,23 @@
-/*
 #include "SerialManager.h"
 
-SerialManager::SerialManager()
+#include <QtSerialPort/QtSerialPort>
+void SerialManager::run()
 {
-}
+	port = new QSerialPort("ttymxc3");
+	port->setBaudRate(115200);
+	port->open(QIODevice::ReadOnly);
+	if(port->isOpen())
+	{
+		qDebug() << "Port ouvert";
+		QByteArray line;
 
-*/
+		forever
+		{
+			//if(port->canReadLine())
+			{
+				qDebug() << "On a un truc à lire";
+				parse(QString(port->readLine(64)));
+			}
+		}
+	}
+}
--- src/SerialManager.h
+++ src/SerialManager.h
@@ -1,35 +1,21 @@
 #ifndef SERIALMANAGER_H
 #define SERIALMANAGER_H
-#include <QtSerialPort/QtSerialPort>
 #include <QThread>
+#include <QString>
+#include <QStringList>
+#include <QDebug>
 
+class QSerialPort;
 class SerialManager : public QThread
 {
 	public:
-		QSerialPort* port;//{QString("ttymxc3")};
+		QSerialPort* port;
 		SerialManager()
 		{
-			//port.setBaudRate(115200);
 		}
 
 	protected:
-		virtual void run()
-		{
-			port = new QSerialPort("ttymxc3");
-			port->setBaudRate(115200);
-			port->open(QIODevice::ReadOnly);
-			if(port->isOpen()) qDebug() << "Port ouvert";
-			QByteArray line;
-
-			forever
-			{
-				if(port->canReadLine())
-				{
-					qDebug() << "On a un truc à lire";
-					parse(QString(port->readLine(64)));
-				}
-			}
-		}
+		virtual void run();
 
 	private:
 		void parse(QString line)
